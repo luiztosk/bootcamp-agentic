413ab99cbb7c6c62ae5221280affeca41b55f628
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-08-19 17:18:54 -0300

    mensagem decodificada corretamente usando o bitMask

--- erick-wendel-websocket/server.mjs
+++ erick-wendel-websocket/server.mjs
@@ -3,6 +3,13 @@ import crypto, { hash, webcrypto } from 'crypto'
 
 const PORT = 1337
 const WEBSOCK_MG_STR_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+const FIRST_BIT = 128
+const SEVEN_BITS_INTEGER_MARKER = 125
+const SIXTEEN_BITS_INTEGER_MARKER = 126
+const SIXTYFOUR_BITS_INTEGER_MARKER = 127
+const MASK_KEY_BYTES_LENGH = 4
+
 const server = createServer((req, res) => {
     res.writeHead(200)
     res.end('hello')
@@ -18,6 +25,25 @@ function onSocketUpgrade(req, socket, head) {
     console.log({ headers })
     // console.log(`example MDN key: ${createSocketAccept('dGhlIHNhbXBsZSBub25jZQ==')}`)
     socket.write(headers)
+    socket.on('readable', () => onSocketReadable(socket))
+}
+
+function onSocketReadable(socket) {
+    // consume optcode (first byte)
+    socket.read(1)
+    const [markerAndPayloadLength] = socket.read(1)
+    const lengthIndicatorInBits = markerAndPayloadLength - FIRST_BIT
+
+    let messageLength = 0
+    if(lengthIndicatorInBits <= SEVEN_BITS_INTEGER_MARKER) {
+        messageLength = lengthIndicatorInBits
+    } else {
+        throw new Error('message too long cant handle')
+    }    
+    const maskKey = socket.read(MASK_KEY_BYTES_LENGH)
+    const encodedMessage = socket.read(messageLength)
+    const decodedMessage = encodedMessage.map((byte, i) => byte ^ maskKey[i % 4]).toString()
+    console.log(`${messageLength} bytes message received:`, decodedMessage)
 }
 
 function prepareHandshakeHeaders(id) {
